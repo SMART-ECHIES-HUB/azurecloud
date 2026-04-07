d12744514cf3346d6960b2f5b8a935db7cbb4aa5
SMART-ECHIES-HUB <sth.connectus@gmail.com>
2026-04-07 19:53:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: '/smart-echies-hub/'
+  base: '/smart-echies-hub/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
